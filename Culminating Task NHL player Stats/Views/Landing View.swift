c49fe33df6ead879b6c7b90ab9cf6ce626f99428
Thomas Chao <Tchao@lcs.on.ca>
2025-04-20 11:33:40 -0400

    Organize files and convert it in to groups

---
+++ Culminating Task NHL player Stats/Views/Landing View.swift
@@ -0,0 +1,8 @@
+//
+//  Landing View.swift
+//  Culminating Task NHL player Stats
+//
+//  Created by Yuk Yeung Chao on 2025-04-20.
+//
+
+import Foundation
---
+++ Culminating Task NHL player Stats/Views/Overall view.swift
@@ -0,0 +1,8 @@
+//
+//  Overall view.swift
+//  Culminating Task NHL player Stats
+//
+//  Created by Yuk Yeung Chao on 2025-04-20.
+//
+
+import Foundation
