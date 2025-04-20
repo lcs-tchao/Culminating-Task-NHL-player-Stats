8b2354662d0964778cc1cd23eef6f2b43bb7abd8
Thomas Chao <Tchao@lcs.on.ca>
2025-04-20 11:01:52 -0400

    Initial Commit

---
+++ Culminating Task NHL player Stats/Culminating_Task_NHL_player_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_Task_NHL_player_StatsApp.swift
+//  Culminating Task NHL player Stats
+//
+//  Created by Yuk Yeung Chao on 2025-04-20.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_Task_NHL_player_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
