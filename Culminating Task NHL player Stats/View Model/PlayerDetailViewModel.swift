f71fa57c10972f38f3b2998cc0a87c9f0fc08c91
Thomas Chao <Tchao@lcs.on.ca>
2025-05-16 14:54:47 -0400

    Have the most basic player view model and player detail view works, and need to add search functions next

--- Culminating Task NHL player Stats/View Model/PlayerDetailViewModel.swift
+++ Culminating Task NHL player Stats/View Model/PlayerDetailViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+
+@Observable
+class PlayerViewModelDetail {
+    
+
+    var currentPlayer: PlayerDetail?
+    
+
+    var favouritePlayer: [PlayerDetail] = []
+
+    init(currentPlayer: PlayerDetail? = nil) {
+        
+
+        self.currentPlayer = currentPlayer
+
+        Task {
+            await self.fetchPlayer()
+        }
+    }
+
+    
+    func fetchPlayer() async {
+
+        let endpoint = "https://api-web.nhle.com/v1/player/8478402/landing"
+        guard let url = URL(string: endpoint) else {
+            print("Invalid address for JSON endpoint.")
+            return
+        }
+
+        do {
+       
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+
+            print("Got data from endpoint, contents of response are:")
+            print(String(data: data, encoding: .utf8)!)
+   
+            let decoder = JSONDecoder()
+ 
+            let decodedData = try decoder.decode([PlayerDetail].self, from: data)
+            
+
+            self.currentPlayer = decodedData.first!
+            
+        } catch {
+            
+
+            print("Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type.")
+            print("----")
+
+            print(error)
+            
+        }
+    }
+    
+}
--- Culminating Task NHL player Stats/View Model/PlayerViewModel.swift
+++ Culminating Task NHL player Stats/View Model/PlayerViewModel.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+@Observable
+class PlayerViewModel {
+
+    var currentPlayer: Player?
+
+    var favouritePlayer: [Player] = []
+    
+ 
+    init(currentPlayer: Player? = nil) {
+
+        self.currentPlayer = currentPlayer
+        
+
+        Task {
+            await self.fetchPlayer()
+        }
+    }
+
+
+    
+    func fetchPlayer() async {
+
+        let endpoint = "https://search.d3.nhle.com/api/v1/search/player?culture=en-us&limit=20&q=mcdavid"
+        guard let url = URL(string: endpoint) else {
+            print("Invalid address for JSON endpoint.")
+            return
+        }
+
+        do {
+
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            print("Got data from endpoint, contents of response are:")
+            print(String(data: data, encoding: .utf8)!)
+
+            let decoder = JSONDecoder()
+  
+            let decodedData = try decoder.decode([Player].self, from: data)
+
+            self.currentPlayer = decodedData.first!
+            
+        } catch {
+
+            print("Count not retrieve data from endpoint, or could not decode into an instance of a Swift data type.")
+            print("----")
+ 
+            print(error)
+            
+        }
+    }
+    
+}
+
