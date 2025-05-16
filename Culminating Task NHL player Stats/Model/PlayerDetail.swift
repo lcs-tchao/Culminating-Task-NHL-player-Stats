8d8b5ce686d378e418826602e5ec21e34b1fba7a
Thomas Chao <Tchao@lcs.on.ca>
2025-05-16 14:39:06 -0400

    complete Player Detail view with JSON decode ( make sure eveything is matched with end point)

--- Culminating Task NHL player Stats/Model/PlayerDetail.swift
+++ Culminating Task NHL player Stats/Model/PlayerDetail.swift
@@ -5,4 +5,55 @@
 //  Created by Yuk Yeung Chao on 2025-05-16.
 //
 
+
 import Foundation
+
+struct RegularSeason: Codable{
+    let subSeason: StatisticsLine
+    let career: StatisticsLine
+}
+
+struct FeaturedStats: Codable {
+    let season: Int
+    let regularSeason: RegularSeason
+}
+struct StatisticsLine: Codable {
+    let assists: Int
+    let gameWinningGoals: Int
+    let gamesPlayed: Int
+    let goals: Int
+    let otGoals: Int
+    let pim: Int
+    let plusMinus: Int
+    let points: Int
+    let powerPlayGoals: Int
+    let powerPlayPoints: Int
+    let shootingPctg: Double
+    let shorthandedGoals: Int
+    let shorthandedPoints: Int
+    let shots: Int
+}
+
+struct FullTeamName: Codable {
+    let english: String
+    let french: String
+   
+    //provide ncoding and decoding hints when pulling data from endpoint
+    enum CodingKeys: String, CodingKey {
+        case english = "default"
+        case french = "fr"
+    }
+}
+
+
+struct PlayerDetail: Identifiable, Codable {
+    
+       let id = UUID()
+    let playerId: Int
+       let isActive: Bool
+       let currentTeamId: Int
+       let currentTeamAbbrev: String
+    let fullTeamName: FullTeamName
+    let featuredStats: FeaturedStats
+      
+}
