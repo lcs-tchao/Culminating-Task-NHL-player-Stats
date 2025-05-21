a8bff54388b8533bef70e78d9fd2414f4699a414
Thomas Chao <Tchao@lcs.on.ca>
2025-05-20 21:25:23 -0400

    Having the 224-25SeasonStats conpleated

--- Culminating Task NHL player Stats/Model/24-25SeasonStats.swift
+++ Culminating Task NHL player Stats/Model/24-25SeasonStats.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct FeaturedStatsCurrentSeason: Codable {
+    let season: Int
+    let regularSeason: RegularSeasonStats
+}
+
+struct RegularSeasonStats: Codable {
+    let subSeason: SubSeasonStats
+}
+
+struct SubSeasonStats: Codable {
+    let assists: Int
+    let gameWinningGoals: Int
+    let gamesPlayed: Int
+    let goals: Int
+    let otGoals: Int
+    let pim: Int
+    let plusMinus: Int
+    let points: Int
+    let powerPlayGoals: Int
+    let powerPlayPoints: Int
+    let shootingPctg: Double
+    let shorthandedGoals: Int
+    let shorthandedPoints: Int
+    let shots: Int
+}
